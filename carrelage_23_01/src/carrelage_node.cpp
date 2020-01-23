b7713f4da882e1994aa803e19db050214651b3e7
CBeaune <57994352+CBeaune@users.noreply.github.com>
2020-01-23 12:04:23 +0100

    Update carrelage_node.cpp

--- carrelage_23_01/src/carrelage_node.cpp
+++ carrelage_23_01/src/carrelage_node.cpp
@@ -1,10 +1,16 @@
 #include <ros/ros.h> 
-#include <geometry_msgs/Position.h>
-using namespace std;
-double x,y,theta
-Quaternion q;
+#include <geometry_msgs/Pose.h>
+#include <std_msgs/Int8.h>
+#include "usefulFunctions.h"
 
-void stateCallBack(geometry_msgs::Pose pose){
+using namespace std;
+
+double x,y,theta;
+Quaternion q;
+int mesures;
+
+
+void odomCallBack(geometry_msgs::Pose pose){
 	x = pose.position.x;
 	y = pose.position.y;
 
@@ -15,22 +21,21 @@ void stateCallBack(geometry_msgs::Pose pose){
 	theta = ToEulerAngles(q).yaw;
 }
 
+
+void sensorCallBack(std_msgs::Int8 sensor){
+	mesures=sensor.data;
+}
+
+
 int main(int argc, char**argv){
 	ros::init(argc,argv,"carrelage");
-	ros::NodeHandle nh;
-	ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::Pose>("/odom/pose/pose",stateCallBack);
-	ros::Subscriber sensor_sub = nh.subscribe<geometry_msgs::Pose>("/odom/pose/pose",stateCallBack);
+	ros::NodeHandle nh("~");
+	ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::Pose>("/odom/pose/pose",1,odomCallBack);
+	ros::Subscriber sensor_sub = nh.subscribe<std_msgs::Int8>("/detect",1,sensorCallBack);
 	
 	while(ros::ok()){
 		
 		ros::spinOnce();
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	}
